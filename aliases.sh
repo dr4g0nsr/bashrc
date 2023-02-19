54e607f5982e5537bf69b9d749448a53fe998dc2
Dragutin Cirkovic <dragonmen@gmail.com>
2023-02-19 19:18:48 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -16,6 +16,7 @@ alias dcd="docker-compose down"
 alias dcu="docker-compose up -d"
 alias dcc="curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 --output /tmp/docker-compose && chmod 777 /tmp/docker-compose && mv /tmp/docker-compose /usr/bin/"
 alias dcp="docker system prune -a"
+alias dps="docker ps -a"
 
 # K8S
 alias ka="kubeadm $@"

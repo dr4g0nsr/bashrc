b27bbd8cc3663c2c5716477377f24647997ed32d
Dragutin Cirkovic <dragonmen@gmail.com>
2023-05-17 21:27:00 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -15,7 +15,7 @@ alias git-reset-all="git reset --hard HEAD^"
 # docker
 alias dcd="docker-compose down"
 alias dcu="docker-compose up -d"
-alias dcc="curl -L https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 --output /tmp/docker-compose && chmod 777 /tmp/docker-compose && mv /tmp/docker-compose /usr/bin/"
+alias dcc="curl -L https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-linux-x86_64 --output /tmp/docker-compose && chmod 777 /tmp/docker-compose && mv /tmp/docker-compose /usr/bin/"
 alias dcp="docker system prune -a"
 alias dps="docker ps -a"
 
